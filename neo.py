d5b2f7aaacc81369b6210bad0bae356f8a59f451
RIS4vis <fibrotech2022@gmail.com>
2026-03-09 20:48:10 +0100

    Create main application file (neo.py) in NEO AI system.

---
+++ neo.py
@@ -0,0 +1,12 @@
+# Main application code
+
+class Neo:
+    def __init__(self):
+        pass
+
+    def run(self):
+        print('NEO AI System is running...')
+
+if __name__ == '__main__':
+    neo = Neo()
+    neo.run()
